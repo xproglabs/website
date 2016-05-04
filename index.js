0224c1944573916eab93f8ce818bb5ec1646efb9
Valdo Raya <cravoley@gmail.com>
2016-05-04 20:59:53 -0300

    Move CSS and JS classes to the bottom of file to make the page load faster

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 // topbar fixo com scroll
 
-$(window).scroll(function() { 
+$(window).scroll(function() {
     var scroll = $(window).scrollTop();
- 
+
     if (scroll > 500) {
         $('.topbar').addClass('topbarFixo');
     } else {
         $('.topbar').removeClass('topbarFixo');
     }
 });
+
+(function($){
+
+    $(window).ready(function(){
+        var currentHash = document.location.hash;
+        if(currentHash && currentHash != "undefined"){
+
+        }
+        var menuContainer = $("nav").find(".navbar-nav");
+        var selectMenu = function(hash){
+            $(menuContainer).find("li").removeClass("active");
+            $(menuContainer).find("li").find("a[href="+hash+"]").parent("li").addClass("active");
+        }
+        $(window).on('hashchange', function(e) {
+            selectMenu(document.location.hash);
+        });
+    });
+})(jQuery)
 
 
 
