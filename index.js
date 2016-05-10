090623bf9d5808bc705f4df8fc80a84fc80555b5
Valdo Raya <cravoley@gmail.com>
2016-05-09 21:12:56 -0300

    Select a menu based on scroll position

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 // topbar fixo com scroll
-
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-
-    if (scroll > 500) {
-        $('.topbar').addClass('topbarFixo');
-    } else {
-        $('.topbar').removeClass('topbarFixo');
-    }
-});
-
 (function($){
 
     $(window).ready(function(){
         var menuContainer = $("nav").find(".navbar-nav");
+        var topMenuHeight = $('.navbar.navbar-fixed-top').height();
         var selectMenu = function(hash){
-            if(hash && hash != "undefined"){
-                $(menuContainer).find("li").removeClass("active");
-                $(menuContainer).find("li").find("a[href="+hash+"]").parent("li").addClass("active");
+            if(!hash || hash == "undefined") hash = "#home";
+            $(menuContainer).find("li").removeClass("active");
+            var toSelect = $(menuContainer).find("li").find("a[href="+hash+"]");
+            if(toSelect.size() > 0){
+                $(toSelect).parent("li").addClass("active");
+                return true;
             }
+            return false;
+            // }
         }
+
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+            // if (scroll > 500) {
+            //     $('.topbar').addClass('topbarFixo');
+            // } else {
+            //     $('.topbar').removeClass('topbarFixo');
+            // }
+
+            $(".slide").each(function(){
+                // console.log($(this).find("h2").eq(0).offset());
+                var padding = parseInt($(this).css("padding-bottom"));
+                var height = parseInt($(this).height());
+                var position = parseInt($(this).offset().top);
+                var endPoint = padding + height + position;
+
+                var visible = (endPoint - scroll - topMenuHeight) > 0 ? true : false;
+                if(visible){
+                    var elementId = $(this).attr("id");
+                    // if found a valid menu item
+                    if(selectMenu("#"+elementId))
+                        return false;
+                }
+
+            });
+        });
+
         $(window).on('hashchange', function(e) {
             selectMenu(document.location.hash);
         });
